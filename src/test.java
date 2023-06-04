245a5cf782dfa0b1a1ce9025ee1dd86d72dc51b4
huilingyu <2388365312@qq.com>
2023-06-04 20:07:39 +0800

    打印12138

--- src/test.java
+++ src/test.java
@@ -1,6 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("nb666");
-        System.out.println(" ");
+        System.out.println("12138");
+
     }
 }
